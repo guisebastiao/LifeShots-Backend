7783380a50ed69f868f867ebe941106fa04ce57c
Guilherme <guilhermesebastiaou.u@gmail.com>
2025-01-24 10:25:11 -0300

    api configurada para rodar na lan

--- src/server.js
+++ src/server.js
@@ -2,4 +2,6 @@ import dotenv from "dotenv";
 import app from "./app";
 dotenv.config();
 
-app.listen(process.env.SERVER_PORT, () => console.log("Server Running"));
+app.listen(process.env.SERVER_PORT, "0.0.0.0", () =>
+  console.log("Server Running")
+);
